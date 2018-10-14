7ea85d9e415b5836ea0fac2ab1a4c62797ce96b2
DESKTOP-P4AOPP7\Diogo <diogozucch@gmail.com>
2018-10-14 18:50:41 -0300

    Versionando fontes existentes.

---
+++ tcc/src/Router.js
@@ -0,0 +1,54 @@
+import { createStackNavigator } from "react-navigation";
+
+import LoginPage from './pages/LoginPage'
+import SeriesPage from './pages/SeriesPage'
+import SerieDetailPage from './pages/SerieDetailPage'
+import SerieFormPage from './pages/SerieFormPage'
+
+export default createStackNavigator({
+	'Login': {
+		screen: LoginPage,
+		navigationOptions: {
+			title: 'Bem vindo',
+		}
+	},
+	'Main': {
+		screen: SeriesPage
+	},
+	'SerieForm': {
+		screen: SerieFormPage,
+		navigationOptions: ({ navigation }) => {
+			if (navigation.state.params && navigation.state.params.serieToEdit) {
+				return {
+					title: navigation.state.params.serieToEdit.title,
+				}
+			}
+			return {
+				title: 'Nova série',
+			};
+		}
+	},
+	'SerieDetail': {
+		screen: SerieDetailPage,
+		navigationOptions: ({ navigation }) => {
+			const { serie } = navigation.state.params;
+			return {
+				title: serie.title,
+			}
+		}
+	},
+}, {
+	navigationOptions: {
+		title: 'Séries!',
+		headerTintColor: 'white',
+		headerStyle: {
+			backgroundColor: '#6ca2f7',
+			borderBottomWidth: 1,
+			borderBottomColor: '#C5C5C5'
+		},
+		headerTitleStyle: {
+			color: 'white',
+			fontSize: 30,
+		}
+	}	
+});
